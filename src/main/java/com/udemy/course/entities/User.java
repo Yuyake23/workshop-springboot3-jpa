81a2255d74bef156a61150fe5630493250303540
Yuyake23 <brunosamuel.orizona@gmail.com>
2022-12-30 13:56:44 -0300

    User.order now is mappedBy client

--- src/main/java/com/udemy/course/entities/User.java
+++ src/main/java/com/udemy/course/entities/User.java
@@ -12,7 +12,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -31,8 +30,7 @@ public class User implements Serializable {
 	private String password;
 
 	@JsonIgnore
-	@OneToMany
-	@JoinColumn(name = "client_id")
+	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
 
 	public User() {
